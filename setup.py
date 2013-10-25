1b713d6ef7e1a60cdeb8f6c17401aba9234aeb04
physacco <physacco@gmail.com>
2013-10-25 10:33:49 +0800

    support pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='zwc',
+  version='0.1.0',
+  description='Chinese word count tool',
+  long_description='This is a simple Chinese word count tool that contains a command line program and a Python module.',
+  url='https://github.com/physacco/zwc',
+  author='physacco',
+  author_email='physacco@gmail.com',
+  license='MIT',
+  py_modules=['zwc'],
+  scripts=['zwc']
+)
